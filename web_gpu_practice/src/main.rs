75ac5cd403a22edbf3a38d8c64f5df251051023c
Eriq Taing <eriq12@protonmail.com>
2024-03-31 13:39:56 -0400

    moved texture code to it's own module

--- web_gpu_practice/src/main.rs
+++ web_gpu_practice/src/main.rs
@@ -1,5 +1,7 @@
 mod tutorial_window;
 
+// using tutorial from https://sotrh.github.io/learn-wgpu/
+
 fn main() {
     pollster::block_on(tutorial_window::run());
 }
---
+++ web_gpu_practice/src/tutorial_window/texture.rs
@@ -0,0 +1,104 @@
+use image::GenericImageView;
+use anyhow::*;
+
+pub struct Texture {
+    pub texture: wgpu::Texture,
+    pub view: wgpu::TextureView,
+    pub sampler: wgpu::Sampler,
+}
+
+impl Texture {
+    pub fn from_bytes(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        bytes: &[u8],
+        label: &str
+    ) -> Result<Self> {
+        let img = image::load_from_memory(bytes)?;
+        Self::from_image(device, queue, &img, Some(label))
+    }
+
+    pub fn from_image(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        img: &image::DynamicImage,
+        label: Option<&str>
+    ) ->Result<Self> {
+        // fix for later version: get rbga, then get data from rgba
+        // the first rgba is to get a longer held time for new rgba data
+        let rgba = img.to_rgba8();
+        let data: &[u8] = rgba.as_raw().as_slice();
+        let dimensions = img.dimensions();
+
+        let size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            // All textures are stored as 3D, we represent our 2D texture
+            // by setting depth to 1.
+            depth_or_array_layers: 1,
+        };
+
+        let texture = device.create_texture(
+            &wgpu::TextureDescriptor {
+                label,
+                size,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                // Most images are stored using sRGB, so we need to reflect that here.
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                // TEXTURE_BINDING tells wgpu that to use this texture in shaders
+                // COPY_DST means that we want to copy data to this texture
+                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+                /*  This is the same as with the SurfaceConfig. It
+                 *  specifies what texture formats can be used to
+                 *  create textureViews for this texture. The base
+                 *  texture format (Rgba8UnormSrgb in this case) is
+                 *  always supported. Note that using a different
+                 *  texture format is not supported on the WebGL2
+                 *  backend.
+                 */
+                view_formats:&[],
+            }
+        );
+
+        queue.write_texture(
+            // Tells wgpu where to copy pixel data
+            wgpu::ImageCopyTexture {
+                aspect: wgpu::TextureAspect::All,
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            // The actual pixel data
+            // before just put rgba, but need to figure out what goes here instead
+            // or more so how to get the desired format: &[u8]
+            // fix shown above, also renamed from rgba to data to stop yelling
+            data,
+            // The layout of the texture, translation of the data above into the image data
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: Some(4 * dimensions.0),
+                rows_per_image: Some(dimensions.1),
+            },
+            size,
+        );
+
+        // We don't need to configure the texture view much,
+        // let wgpu define it.
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let sampler = device.create_sampler(
+            &wgpu::SamplerDescriptor {
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
+                mag_filter: wgpu::FilterMode::Linear,
+                min_filter: wgpu::FilterMode::Nearest,
+                mipmap_filter: wgpu::FilterMode::Nearest,
+                ..Default::default()
+            }
+        );
+
+        Ok(Self { texture, view, sampler })
+    }
+}
